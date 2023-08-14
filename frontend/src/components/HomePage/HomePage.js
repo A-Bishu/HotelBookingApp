de01cb2453159b5730360cae11ee66b57f3fa176
Ashley Ferenchak <ashleyf01@me.com>
2023-08-14 19:57:36 -0400

    Fixing a few warnings and errors

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import './HomePage.css'
 
+function myFunction() {}
+
 export function HomePage (){
     return (
         <>
             <div className='background'>
                 <h2 className="title">Welcome to Hotel Booking</h2>
-                <input type="text" id="searchbar" onkeyup="myFunction()" placeholder="Search for hotels.." ></input>
+                <input type="text" id="searchbar" onKeyUp={myFunction} placeholder="Search for hotels.." ></input>
                 <button type="submit">Search</button>
             </div>
         </>
--- frontend/src/components/ReservationForm/ReservationForm.js
+++ frontend/src/components/ReservationForm/ReservationForm.js
@@ -4,42 +4,42 @@ export function ReservationForm (){
         <h3>Reservation Form</h3>
         <form className="reservation-form" action="reservation.php" method="post">
           <div>
-            <label for="name">Your Name</label>
+            <label htmlFor="name">Your Name</label>
             <input type="text" id="name" required/>
           </div>
           <div>
-            <label for="email">Your E-mail</label>
+            <label htmlFor="email">Your E-mail</label>
             <input type="email" id="email" required/>
           </div>
           <div>
-            <label for="phone">Your Phone</label>
+            <label htmlFor="phone">Your Phone</label>
             <input type="tel" id="phone" required/>
           </div>
           
           <div>
-            <label for="adult">Adults</label>
+            <label htmlFor="adult">Adults</label>
             <input type="number" id="adult" min="1" required/>
           </div>
           <div>
-            <label for="child">Children</label>
+            <label htmlFor="child">Children</label>
             <input type="number" id="child" min="0" required/>
           </div>
           <div>
-            <label for="checkin-date">Check-in Date</label>
+            <label htmlFor="checkin-date">Check-in Date</label>
             <input type="date" id="checkin-date" required/>
           </div>
           <div>
-            <label for="checkout-date">Check-out Date</label>
+            <label htmlFor="checkout-date">Check-out Date</label>
             <input type="date" id="checkout-date" required/>
           </div>
           <div>
-            <label for="room-selection">Select room preference</label>
+            <label htmlFor="room-selection">Select room preference</label>
             <select id="room-selection" required>
                 <option value="">List of rooms</option>
             </select>
           </div>
           <div>
-            <label for="reversavation-total">Reversavation Total: $</label>
+            <label htmlFor="reversavation-total">Reversavation Total: $</label>
             <input type="number" id="reversavation-total"/>
           </div>
         <button className="cancel-confirm-btn" type="submit">Cancel</button>
