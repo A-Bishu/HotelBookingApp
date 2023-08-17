820dda43ef06f16475ecb8a20c7a7f8ffc17b4d1
Katie Krakue <katiek@ks-air.myfiosgateway.com>
2023-08-16 20:32:28 -0400

    fixed mistake

--- frontend/src/components/HotelList/HotelList.js
+++ frontend/src/components/HotelList/HotelList.js
@@ -1,11 +1,11 @@
 export function HotelList (){
     return (
-         <lu id='list'>
+         <li id='list'>
                 <li>Hermosa Inn</li>
                 <li>Hotel Congress</li>
                 <li>Hotel San Carlos</li>
                 <li>Hotel Valley Ho</li>
-        </lu>
+        </li>
     )
 }
 
