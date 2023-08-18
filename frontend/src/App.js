8019f0c39349b096766acd900f89410a3159d7ed
A-Bishu <bishuk001@gmail.com>
2023-08-18 17:18:52 -0500

    updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
+
 import React, { useEffect, useState } from 'react'
 import  HomePage  from './components/HomePage';
 import { HotelList } from './components/HotelList';
 import apiConn from './components/apiConn';
 
-=======
-import React from 'react'
-import './App.css';
-import { ReservationForm } from './components/ReservationForm/ReservationForm';
-import { HomePage } from './components/HomePage/HomePage';
-import { HotelList } from './components/HotelList/HotelList';
->>>>>>> ab12e7eede11f2f991b21901bb5112d7cb42e2ca
 
 function App() {
 
@@ -35,16 +28,9 @@ function App() {
   return (
     <div className="App">
       <HomePage />
-<<<<<<< HEAD
-   
-      <HotelList hotels={hotels} /> {/* Pass the fetched hotels data as a prop */}
-=======
-      <HotelList />
-      {/* Put reservation at the bottom so it displays last KK */}
-      <ReservationForm /> 
->>>>>>> ab12e7eede11f2f991b21901bb5112d7cb42e2ca
+      <HotelList hotels={hotels} />
     </div>
   );
-}
+};
 
 export default App;
