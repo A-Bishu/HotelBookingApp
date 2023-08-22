40949c144dc7efffc52fee8d1cb3f59bdd93ae23
Katie Krakue <>
2023-08-21 20:22:40 -0400

    updated application

--- frontend/src/components/HomePage.js
+++ frontend/src/components/HomePage.js
@@ -12,3 +12,23 @@ const HomePage = () => {
 };
 
 export default HomePage;
+
+// import './HomePage.css'
+// import HotelDropdown from '../DropdownList/DropdownList'
+
+// function myFunction() {}
+
+// export function HomePage (){
+//     return (
+//         <>
+//             <div className='background'>
+//                 <h2 className="title">Welcome to Hotel Booking</h2>
+//                 <HotelDropdown />
+                
+//                 {/* Commenting out this search function to try out the dropdown menu */}
+//                 {/* <input type="text" id="searchbar" onKeyUp={myFunction} placeholder="Search for hotels.." ></input>
+//                 <button type="submit">Search</button> */}
+//             </div>
+//         </>
+//     )
+// }
